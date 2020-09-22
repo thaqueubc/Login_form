1b26999db6abad6534c8f6dc5bc8c2a7f1c641ba
thaqueubc <reya.ruet@gmail.com>
2020-09-22 11:26:35 -0700

    Keystroke is printed to the console

--- scripts/validation.js
+++ scripts/validation.js
@@ -9,20 +9,32 @@ form.addEventListener('submit', (e) => {
     messages.push('Name is required')
   }
 
-  if (password.value.length <= 6) {
-    messages.push('Password must be longer than 6 characters')
+  if (name.value.length < 2) {
+    messages.push('Name must be longer than 2 characters');
   }
 
-  if (password.value.length >= 20) {
-    messages.push('Password must be less than 20 characters')
+  if (password.value === '' || password.value == null) {
+    messages.push('Password is required')
   }
+
+  if (password.value.length <= 4) {
+    messages.push('Password must be longer than 4 characters');
+  }
+
+  // if (password.value.length >= 20) {
+  //   messages.push('Password must be less than 20 characters');
+  // }
 
   if (password.value === 'password') {
-    messages.push('Password cannot be password')
+    messages.push('Password cannot be password');
   }
 
   if (messages.length > 0) {
     e.preventDefault()
-    errorElement.innerText = messages.join(', ')
+    errorElement.innerText = messages.join('\n ')
   }
 })
+
+function printName(n){
+  console.log(n);
+}
