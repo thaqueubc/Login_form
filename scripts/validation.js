3631c984f368dac683d55f8aa6ef49fe5e4905f9
thaqueubc <reya.ruet@gmail.com>
2020-09-23 13:19:07 -0700

    logic for preventing double click is added

--- scripts/validation.js
+++ scripts/validation.js
@@ -2,16 +2,21 @@ const name = document.getElementById('name');
 const password = document.getElementById('password');
 const form = document.getElementById('login-form');
 const errorElement = document.getElementById('error');
-//const welcomeMessage = document.getElementById('welcome');
+let click = 0;
+
+function printName(n){
+  console.log(n);
+}
 
 function validation(){
   form.addEventListener('click', (e) => {
+    click++;
     let messages = []
     if (name.value === '' || name.value == null) {
       messages.push('Name is required')
     }
   
-    if (name.value.length < 2) {
+    if (name.value.length <= 2) {
       messages.push('Name must be longer than 2 characters');
     }
   
@@ -31,7 +36,7 @@ function validation(){
       e.preventDefault()
       errorElement.innerText = messages.join('\n ')
     }
-    if(messages.length == 0){
+    if(messages.length == 0 && click == 1){
       const url = 'http://dummy.restapiexample.com/api/v1/employees';
     
       fetch(url, {
@@ -59,32 +64,4 @@ function validation(){
   })
   
 }
-function printName(n){
-  console.log(n);
-}
 
-// function signIn(){
-//    const url = 'http://dummy.restapiexample.com/api/v1/employees';
-    
-//   fetch(url, {
-//   method: 'GET'
-//   })
-//   .then(response => response.json())
-//   .then((result) => {
-//     console.log('Success:', result);
-    
-//     const users = result.data;
-//     users.forEach(data => {
-//       // console.log(data.employee_name);
-//        if(data.employee_name == name.value){
-//          console.log("Found");
-//         welcomeMessage.innerText = "Welcome" + name.value;
-//          location.href = "welcome.html";
-//        }
-//     });
-
-//   })
-//   .catch(error => {
-//     console.error('Error:', error);
-//   });
-// }
